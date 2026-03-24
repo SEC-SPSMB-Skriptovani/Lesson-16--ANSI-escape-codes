7f16515b5b1225e14ed3d66f82307e35e6644753
David Šec <144169715+SPSMB-DavidSec@users.noreply.github.com>
2026-03-24 19:05:43 +0100

    Add script to display colored text in terminal

---
+++ test-sctipt-with-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Load color definitions
+source ./colors.sh
+
+
+echo -e "${GREEN}This is green text${RESET}"
+echo -e "${RED}${BOLD}Error message!${RESET}"
+echo -e "${BG_BLUE}${WHITE} Blue background ${RESET}"
